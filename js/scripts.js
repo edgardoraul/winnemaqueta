fc02281b8bfe69219520a142704df54e292108f7
edgardoraul <edgardo_raul@hotmail.com>
2021-03-27 19:02:20 -0300

    Quetado las cosas innecesarias del sass autoprefixer. Correcta estilización e interacción con el menú principal.

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,21 @@
+// Escuchando el evento para que cargue todo el DOM
 window.addEventListener("DOMContentLoaded", cargarScripts);
 
+// La función que carga los scripts de funciones importantes.
 function cargarScripts()
 {
-	// Funcionalidad al botón del menú
+	/* El menú principal **********************/
+
+	// Variable del botón principal que abre y ciera el menú
 	const botonMenu = document.querySelector("#boton__menu");
-	const listadoSubMenu = document.querySelectorAll(".navegacion__lista__item__enlace + .sublista");
+
+	// Variable que contiene el listado de menú principal de la web
 	const listadoMenu = document.querySelector("#header_nav");
 
-	// Creando botones clickeables para abrir los submenúes
-	const listadoSubMenuBoton = [];
-
-	listadoSubMenu.forEach( (ev) => {
-		listadoSubMenuBoton.push( ev.previousElementSibling);
-	});
-
-
-	for (let i = 0; i < listadoSubMenuBoton.length; i++ )
-	{
-		listadoSubMenuBoton[i].addEventListener("click", (ev) => {
-			ev.preventDefault();
-
-			if( listadoSubMenuBoton[i].nextElementSibling.classList.contains("sublista--abierto") )
-			{
-				listadoSubMenuBoton[i].nextElementSibling.classList.remove("sublista--abierto");
-				listadoSubMenuBoton[i].nextElementSibling.classList.add("sublista--cerrado");
-			}
-			
-			else if( listadoSubMenuBoton[i].nextElementSibling.classList.contains("sublista--cerrado") )
-			{
-				listadoSubMenuBoton[i].nextElementSibling.classList.add("sublista--abierto");
-				listadoSubMenuBoton[i].nextElementSibling.classList.remove("sublista--cerrado");
-			}
-
-			else
-			{
-				listadoSubMenuBoton[i].nextElementSibling.classList.add("sublista--abierto");
-				listadoSubMenuBoton[i].nextElementSibling.classList.remove("sublista--cerrado");
-			}
-		});
-	}
-
+	// Escuchando el evento de click del botón del menú principal
 	botonMenu.addEventListener( "click", abrirCerrar );
 
-	
+	// Función que puede abrir y cerrar el menú principal
 	function abrirCerrar(ev)
 	{
 		ev.preventDefault();
@@ -64,5 +37,61 @@ function cargarScripts()
 			listadoMenu.classList.add("navegacion__lista--abierto");
 			listadoMenu.classList.remove("navegacion__lista--cerrado");
 		}
+	}
+
+
+	/* Los submenúes secundario del menú principal ************/
+
+	// Variable de los listados de sub menúes. Es un arreglo
+	const listadoSubMenu = document.querySelectorAll(".navegacion__lista__item__enlace + .sublista");
+
+	// Creando botones clickeables para abrir los submenúes. Es un arreglo.
+	const listadoSubMenuBoton = [];
+	listadoSubMenu.forEach( (ev) => {
+		listadoSubMenuBoton.push( ev.previousElementSibling);
+	});
+
+	// Se le asigna la propiedad de poder abrir y cerrar a cada enlace que funcionará como botón de submenú.
+	for (let i = 0; i < listadoSubMenuBoton.length; i++ )
+	{
+		listadoSubMenuBoton[i].addEventListener("click", (ev) => {
+			ev.preventDefault();
+
+			if( listadoSubMenuBoton[i].nextElementSibling.classList.contains("sublista--abierto") )
+			{
+				listadoSubMenuBoton[i].nextElementSibling.classList.remove("sublista--abierto");
+				listadoSubMenuBoton[i].nextElementSibling.classList.add("sublista--cerrado");
+
+				if(listadoSubMenuBoton[i].childNodes[0].nextElementSibling.classList.contains("icon-minus"))
+				{
+					listadoSubMenuBoton[i].childNodes[0].nextElementSibling.classList.add("icon-plus");
+					listadoSubMenuBoton[i].childNodes[0].nextElementSibling.classList.remove("icon-minus");
+				}
+			}
+			
+			else if( listadoSubMenuBoton[i].nextElementSibling.classList.contains("sublista--cerrado") )
+			{
+				listadoSubMenuBoton[i].nextElementSibling.classList.add("sublista--abierto");
+				listadoSubMenuBoton[i].nextElementSibling.classList.remove("sublista--cerrado");
+				
+				if(listadoSubMenuBoton[i].childNodes[0].nextElementSibling.classList.contains("icon-plus"))
+				{
+					listadoSubMenuBoton[i].childNodes[0].nextElementSibling.classList.add("icon-minus");
+					listadoSubMenuBoton[i].childNodes[0].nextElementSibling.classList.remove("icon-plus");
+				}
+			}
+			
+			else
+			{
+				listadoSubMenuBoton[i].nextElementSibling.classList.add("sublista--abierto");
+				listadoSubMenuBoton[i].nextElementSibling.classList.remove("sublista--cerrado");
+
+				if(listadoSubMenuBoton[i].childNodes[0].nextElementSibling.classList.contains("icon-plus"))
+				{
+					listadoSubMenuBoton[i].childNodes[0].nextElementSibling.classList.add("icon-minus");
+					listadoSubMenuBoton[i].childNodes[0].nextElementSibling.classList.remove("icon-plus");
+				}
+			}
+		});
 	}
 }
