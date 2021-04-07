5d94d76fca5918a52cec5589ccee2cbfd7324a92
edgardoraul <edgardo_raul@hotmail.com>
2021-04-06 22:38:08 -0300

    Construcción del slider. Pero no funciona, tiene errores.

--- js/scripts.js
+++ js/scripts.js
@@ -185,4 +185,80 @@ function cargarScripts()
 		}
 		lastScrollTop = st;
 	}
+
+
+
+
+	/* Las funciones para manipular los sliders */
+	// window.addEventListener("DOMContentLoaded", cargador);
+
+	function cargadorSlider()
+	{
+		console.log("cargado");
+		// El arreglo con cada uno de los sliders.
+		const slidersItems = document.querySelectorAll(".slider__item");
+		// const slidersIframes = document.getElementsByTagName("iframe");
+
+		// Animación de entrada y salida
+		let animacionEntrada, animacionSalida;
+		
+		// Agregando los z-index de acuerdo a su orden
+		for( let i = 0; i < slidersItems.length; i++ )
+		{
+			// slidersItems[i].style.zIndex = slidersItems.length - i;
+
+			// Controlando si tiene una animación o no
+			if( slidersItems[i].classList.contains("animacion__carrousel") )
+			{
+				slidersItems[i].style.order = i + 1;
+				animacionEntrada = "slideInRight";
+				animacionSalida = "slideOutLeft";
+			}
+			else if( slidersItems[i].classList.contains("animacion__fade") )
+			{
+				animacionEntrada = "fadeIn";
+				animacionSalida = "fadeOut";
+			}
+		}
+
+		/* // Agrandando los iframes
+		for( let i = 0; i < slidersIframes.length; i++ )
+		{
+			slidersIframes[i].height = "100%";
+			slidersIframes[i].width = "auto";
+		} */
+
+
+		
+		// Función que se autoejecuta cada 3 segundos
+		let i = 0;
+		function temporizadorInfinito()
+		{
+			setTimeout(temporizadorInfinito, 5000);
+			izq();
+
+			// Control del contador
+			i = ( i + 1 ) % slidersItems.length;
+			der();
+			console.log(slidersItems[i]);
+		}
+		temporizadorInfinito();
+
+		// Desplazamiento hacia la izquierda y desaparece
+		function izq()
+		{
+			slidersItems[i].classList.add(animacionSalida);
+			slidersItems[i].classList.remove(animacionEntrada);
+			slidersItems[i].style.order = i;
+		}
+
+		// Desplazamiento desde la derecha y se queda en el centro 
+		function der()
+		{
+			slidersItems[i].classList.add(animacionEntrada);
+			slidersItems[i].classList.remove(animacionSalida);
+			slidersItems[i].style.order = 1;
+		}
+	}
+	cargadorSlider();
 }
